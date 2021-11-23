18eccaa72452199484a0be8d71f9095b7ca101d2
Fachrul Rozi Hamonangan Harahap <85205921+fachrul-rh@users.noreply.github.com>
2021-11-23 13:24:00 +0700

    change package

--- Project-HORI/webpack.common.js
+++ Project-HORI/webpack.common.js
@@ -1,12 +1,12 @@
-const HtmlWebpackPlugin = require('html-webpack-plugin')
-const path = require('path')
-const webpack = require('webpack')
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+const webpack = require('webpack');
 
 module.exports = {
   entry: './src/app.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js'
+    filename: 'bundle.js',
   },
   module: {
     rules: [
@@ -14,25 +14,25 @@ module.exports = {
         test: /\.css$/,
         use: [
           {
-            loader: 'style-loader'
+            loader: 'style-loader',
           },
           {
-            loader: 'css-loader'
-          }
-        ]
-      }
-    ]
+            loader: 'css-loader',
+          },
+        ],
+      },
+    ],
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: './src/index.html',
-      filename: 'index.html'
+      template: './src/templates/index.html',
+      filename: 'index.html',
     }),
     new webpack.ProvidePlugin({
       $: 'jquery',
       jQuery: 'jquery',
       'window.jQuery': 'jquery',
-      Popper: ['popper.js', 'default']
-    })
-  ]
-}
+      Popper: ['popper.js', 'default'],
+    }),
+  ],
+};
