7bbba3d038634c8e049335b50af960d65e9e67cd
muanra <zirodf21@gmail.com>
2021-11-23 09:49:52 +0700

    add app-bar and footer with bootstrap

--- Project-HORI/src/app.js
+++ Project-HORI/src/app.js
@@ -1,1 +1,8 @@
-console.log("hello wooooooooooooooooorld");
+import './script/component/foot-er'
+import './script/component/app-bar'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/js/bootstrap.min.js'
+import './styles/appBar-style/Brands.css'
+import './styles/appBar-style/dh-navbar-centered-brand.css'
+import './styles/footer-style/bottom.css'
+import './styles/appBar-style/navigation.css'
--- Project-HORI/src/script/component/foot-er.js
+++ Project-HORI/src/script/component/foot-er.js
@@ -1,1 +1,21 @@
-console.log("hello wooooooooooooooooorld");
+class Footer extends HTMLElement {
+    connectedCallback () {
+      this.render()
+    }
+  
+    render () {
+      this.innerHTML = `
+      <footer>
+      <div class="d-flex flex-column justify-content-center align-items-center downfooter">
+          <p class="text-md-left text-lg-right">All data made available by <span><a href="https://github.com/satyawikananda/rs-bed-covid-indo-api/" target="_blank">RS Bed COVID API</a></span></p>
+          <p class="text-md-left text-lg-right" id="downfooter">&nbsp;Made by ❤️&nbsp; in Indonesia&nbsp;</p>
+      </div>
+      <script src="assets/js/jquery.min.js"></script>
+      <script src="assets/bootstrap/js/bootstrap.min.js"></script>
+  </footer>
+          `
+    }
+  }
+  
+  customElements.define('foot-er', Footer)
+  
